1795cc3b5dc505048b41b677b480d4c7771188ed
Gcinizwe Dlamini <gcinizwedlamini@gcinizwes-macbook-air.local>
2018-08-04 10:07:59 +0300

    Initial Commit

---
+++ Data_Structues/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data_Structues
+//
+//  Created by Gcinizwe Dlamini on 04/08/2018.
+//  Copyright © 2018 Gcinizwe Dlamini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
